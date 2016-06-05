df0b2e10189528a25f8f09bb3323283ca932ef8f
Jamie Diprose <jamie.diprose@gmail.com>
2016-06-05 19:59:51 +1200

    Remove dead code.

--- client/main.js
+++ client/main.js
@@ -125,16 +125,3 @@ if (Meteor.isClient) {
         angular.element(document).ready(onReady);
     }
 }
-
-
-
-// if (Meteor.isCordova) {
-//     angular.element(document).on('deviceready', onReady);
-// }
-// else {
-//     angular.element(document).ready(onReady);
-// }
-//
-// function onReady() {
-//     angular.bootstrap(document, ['tangibles'], {strictDi: true});
-// }
