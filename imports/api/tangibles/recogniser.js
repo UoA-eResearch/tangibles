e6781ad4b4372e4380dc42e1d4396a9f9fa32967
Jamie Diprose <jamie.diprose@gmail.com>
2016-07-17 21:31:17 +1200

    Algorithm documentation.

--- imports/api/tangibles/recogniser.js
+++ imports/api/tangibles/recogniser.js
@@ -20,9 +20,15 @@ export class Recogniser {
     }
 
     /**
+     * The recognition algorithm computes the similarity between a triangle (three touch points) and an array of features,
+     * each feature being three touch points that represent a particular tangible.
+     *
+     * To consistently compare triangles, the algorithm finds the centroid of each triangle, and then finds the point
+     * with the furthest distance from the centre. It then sorts the points that make up each triangle clockwise
+     * and computes the similarity of each triangle by seeing how closely the lengths of each side match.
      *
      * @param points: a feature to predict a tangible from, a set of three touch points.
-     * @returns []
+     * @returns []: an array of targets and their similarity to the detected touch points.
      */
 
     predict(points) {
@@ -30,7 +36,11 @@ export class Recogniser {
 
         // We need to have minimum of three touch points to identify the Visual and it's orientation.
         if (points.length == 3) {
-            let touchSorted = Points.sortClockwise(points);
+            let touchSorted = Points.sortClockwise(points); // Sort the detected points in clockwise order. The point
+            // with the furthest distance from the centroid of the triangle is first. We do this so that when we
+            // consistently compare the lengths of the triangle sides.
+
+            // Find the length of each side of the detected triangle
             let touchDistA = Point.distance(touchSorted[0], touchSorted[1]);
             let touchDistB = Point.distance(touchSorted[0], touchSorted[2]);
             let touchDistC = Point.distance(touchSorted[1], touchSorted[2]);
@@ -39,15 +49,23 @@ export class Recogniser {
                 let feature = this.features[i];
 
                 if (feature.length == 3) {
-                    let regPointsDists = Points.sortClockwise(feature);
+                    let regPointsDists = Points.sortClockwise(feature); // Sort the features points in clockwise order.
+                    // The point with the furthest distance from the centroid of the triangle is first. We do this so
+                    // that when we consistently compare the lengths of the triangle sides.
+
+                    // Find the length of each side of the feature triangle
                     let regDistA = Point.distance(regPointsDists[0], regPointsDists[1]);
                     let regDistB = Point.distance(regPointsDists[0], regPointsDists[2]);
                     let regDistC = Point.distance(regPointsDists[1], regPointsDists[2]);
+
+                    // Compute the similarity metric: The sum of the differences between the sides of the touch point
+                    // triangle and the feature triangle.
                     let similarity = Math.abs(touchDistA - regDistA) + Math.abs(touchDistB - regDistB) + Math.abs(touchDistC - regDistC);
                     matches.push({target: this.targets[i], similarity: similarity});
                 }
             }
 
+            // Sort the matches by their similarity. Highest similarity comes first.
             matches.sort(function (a, b) {
                 if (a.similarity < b.similarity)
                     return -1;
